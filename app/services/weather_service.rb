d30f8625b63ca28aa4b049b791f662d713fa8cae
Hoa Dam <hoangocdam@gmail.com>
2024-04-21 15:28:42 -0700

    consume weather API

---
+++ app/services/weather_service.rb
@@ -0,0 +1,15 @@
+class WeatherService
+  def self.conn
+    Faraday.new(url: "http://api.weatherapi.com/v1")
+  end
+
+  def self.get_url(url, params)
+    response = conn.get(url, { key: Rails.application.credentials[:weather_api_key] }.merge(params) )
+    JSON.parse(response.body, symbolize_names: true)
+  end
+
+  def self.get_weather(lat, lng)
+    response = get_url("forecast.json", q: "#{lat},#{lng}", days: 5)
+      Weather.from_json(response)
+  end
+end
