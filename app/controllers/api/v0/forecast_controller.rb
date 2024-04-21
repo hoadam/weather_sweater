279e6a40ab2fa8a52157f53a64f9dd65a96ea8a5
Hoa Dam <hoangocdam@gmail.com>
2024-04-21 15:32:03 -0700

    feat: forecast API

---
+++ app/controllers/api/v0/forecast_controller.rb
@@ -0,0 +1,10 @@
+class Api::V0::ForecastController < ApplicationController
+
+  def index
+    location = MapQuestService.get_latlng(params[:location])
+    lat = location.lat
+    lng = location.lng
+    weather_data = WeatherService.get_weather(lat, lng)
+    render json: ForecastSerializer.format_data(weather_data)
+  end
+end
