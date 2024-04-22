20372bf7b18d040e0080c5efa49c5dd7ec86a8cb
Hoa Dam <hoangocdam@gmail.com>
2024-04-22 08:04:13 -0700

    feat: routes

--- config/routes.rb
+++ config/routes.rb
@@ -10,6 +10,8 @@ Rails.application.routes.draw do
   namespace :api do
     namespace :v0 do
       resources :forecast, only: [:index]
+      resources :sessions, only: [:create]
+      resources :users, only: [:create]
     end
   end
 end
