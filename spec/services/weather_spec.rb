be43590ce2f73e1ce99292d94154c241ed5fe061
Hoa Dam <hoangocdam@gmail.com>
2024-04-23 00:05:28 -0700

    feat:weather at end point test

--- spec/services/weather_spec.rb
+++ spec/services/weather_spec.rb
@@ -12,4 +12,16 @@ RSpec.describe WeatherService do
       expect(weather.current.last_updated).to eq("2024-04-21 14:30")
     end
   end
+
+  describe '.get_end_point_weather' do
+    it "returns weather data for the end point of the trip", :vcr do
+      location = "Oakland, CA"
+      datetime = "2024-04-22"
+
+      end_point_weather = WeatherService.get_end_point_weather(location, datetime)
+
+      expect(end_point_weather).to be_a(Weather::Forecast)
+      expect(end_point_weather.date).to eq(datetime)
+    end
+  end
 end
